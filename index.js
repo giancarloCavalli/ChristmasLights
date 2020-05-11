55486368cc36ac90e5b0bf2d9cf090e14e09d5da
giancarloCavalli <gc.giancarloo@gmail.com>
2020-05-10 21:59:03 -0300

    Inclusão generateLights()

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var circ = document.getElementsByClassName("circle");
 setLights(0);   //inicia o pisca pisca
 setTimeout(function() { setLights(1); }, 600)   // inicia o pisca pisca
+generateLights()
 
 
 function startStop() {
@@ -38,17 +39,45 @@ function setDuration() {
 
 function generateLights() {
     var lContainer = document.getElementById("lightsContainer")
-    var qtLights = document.getElementById("inpQtLights").value
+    var qtLights = document.getElementById("inpQtLights")
     lContainer.innerHTML=""
-    for(i=0; i<qtLights; i++) {
+    for(i=0; i<qtLights.value; i++) {
         var circ = document.createElement("div")
         circ.setAttribute("class", "circle")
+        circ.setAttribute("id",`c${i}`)
         lContainer.append(circ)
     }
+    generateCheckList(qtLights.value)
     setLights(0);   // inicia o pisca pisca
     setTimeout(function() { setLights(1); }, 600) // inicia o pisca pisca
+    qtLights.value = ""
 }
 
 function setColor() {
-    
+    var color = document.getElementById("colorSelector")
+    var lightsCollection = document.getElementsByClassName("circle")
+    var checkCollection = document.getElementsByClassName("chBox")
+    for(i=0; i<checkCollection.length; i++) {
+        if(checkCollection[i].checked) {
+            var light = lightsCollection[i].style.animation
+            //light.style.backgroundColor = "blue"
+            alert(light.style.background)
+            //lightsCollection[i].style.backgroundColor=`${color.value}`
+        }
+    }
+}
+
+function generateCheckList(len) {
+    var checkL = document.getElementById("lightsCheckList")
+    checkL.innerHTML=""
+    for(i=0; i<len; i++) {
+        var lItem = document.createElement("li")
+        var checkB = document.createElement("INPUT")
+        checkB.setAttribute("type","checkbox")
+        checkB.setAttribute("id",`ch${i}`)
+        checkB.setAttribute("class","chBox")
+        lItem.innerText = `c${i}`
+        lItem.append(checkB)
+        checkL.append(lItem)
+    }
 }
